44478825011e9e43fa1ad99c11b8819623de8b88
Mirkopoj <mirkopoj@hotmail.com>
2022-11-02 23:02:48 -0300

    Primer commit

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+type TcpMessage = [u8;1];
+
+#[derive(Copy, Clone)]
+pub struct DataStruct {
+    pub cinta: bool,
+    pub pogos: bool,
+    pub selector: bool,
+    pub sensor: bool,
+}
+
+pub fn to_bytes(struct_in: DataStruct) -> TcpMessage {
+    let mut bytes_ret: TcpMessage = [0;1];
+    if struct_in.cinta      { bytes_ret[0] |= 0x01; };
+    if struct_in.pogos      { bytes_ret[0] |= 0x02; };
+    if struct_in.selector   { bytes_ret[0] |= 0x04; };
+    if struct_in.sensor     { bytes_ret[0] |= 0x08; };
+
+    bytes_ret
+}
+
+pub trait Convert {
+    fn to_bytes(&self) -> TcpMessage;
+    
+}
+impl Convert for DataStruct {
+    fn to_bytes(&self) -> TcpMessage {
+        let mut bytes_ret: TcpMessage = [0;1];
+        if self.cinta      { bytes_ret[0] |= 0x01; };
+        if self.pogos      { bytes_ret[0] |= 0x02; };
+        if self.selector   { bytes_ret[0] |= 0x04; };
+        if self.sensor     { bytes_ret[0] |= 0x08; };
+
+        bytes_ret
+    }
+}
+
+pub fn from_bytes(bytes_in: &[u8]) -> DataStruct {
+    let struct_ret = DataStruct {
+        cinta:      (bytes_in[0] & 0x01) != 0 ,
+        pogos:      (bytes_in[0] & 0x02) != 0 ,
+        selector:   (bytes_in[0] & 0x04) != 0 ,
+        sensor:     (bytes_in[0] & 0x08) != 0 ,
+    };
+
+    struct_ret
+}
