8cc0ac711876b11820cef64fd0a5f261ef180549
Mirkopoj <mirkopoj@hotmail.com>
2022-11-10 15:29:48 -0300

    cinta2 y sensor2

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,22 @@ pub const EMPTYTCPMESSAGE: TcpMessage = [0;TCPMESSAGELEN];
 
 #[derive(Copy, Clone,PartialEq)]
 pub struct DataStruct {
-    pub cinta: bool,
+    pub cinta1: bool,
+    pub cinta2: bool,
     pub pogos: bool,
     pub selector: bool,
-    pub sensor: bool,
+    pub sensor1: bool,
+    pub sensor2: bool,
 }
 
 pub fn to_bytes(struct_in: DataStruct) -> TcpMessage {
     let mut bytes_ret: TcpMessage = [0;1];
-    if struct_in.cinta      { bytes_ret[0] |= 0x01; };
-    if struct_in.pogos      { bytes_ret[0] |= 0x02; };
-    if struct_in.selector   { bytes_ret[0] |= 0x04; };
-    if struct_in.sensor     { bytes_ret[0] |= 0x08; };
+    if struct_in.cinta1     { bytes_ret[0] |= 0x01; };
+    if struct_in.cinta2     { bytes_ret[0] |= 0x02; };
+    if struct_in.pogos      { bytes_ret[0] |= 0x04; };
+    if struct_in.selector   { bytes_ret[0] |= 0x08; };
+    if struct_in.sensor1    { bytes_ret[0] |= 0x10; };
+    if struct_in.sensor2    { bytes_ret[0] |= 0x20; };
 
     bytes_ret
 }
@@ -32,10 +36,12 @@ impl Convert for DataStruct {
 
 pub fn from_bytes(bytes_in: &[u8]) -> DataStruct {
     let struct_ret = DataStruct {
-        cinta:      (bytes_in[0] & 0x01) != 0 ,
-        pogos:      (bytes_in[0] & 0x02) != 0 ,
-        selector:   (bytes_in[0] & 0x04) != 0 ,
-        sensor:     (bytes_in[0] & 0x08) != 0 ,
+        cinta1:      (bytes_in[0] & 0x01) != 0 ,
+        cinta2:      (bytes_in[0] & 0x02) != 0 ,
+        pogos:      (bytes_in[0] & 0x04) != 0 ,
+        selector:   (bytes_in[0] & 0x08) != 0 ,
+        sensor1:     (bytes_in[0] & 0x10) != 0 ,
+        sensor2:     (bytes_in[0] & 0x20) != 0 ,
     };
 
     struct_ret
