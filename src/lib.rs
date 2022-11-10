d0ad2bfa091481e1579ef8a1540642f9c82c8040
Mirkopoj <mirkopoj@hotmail.com>
2022-11-10 17:35:54 -0300

    serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+use serde_derive::{Serialize, Deserialize};
+use serde_json;
 const TCPMESSAGELEN: usize = 1;
 pub type TcpMessage = [u8;TCPMESSAGELEN];
 pub const EMPTYTCPMESSAGE: TcpMessage = [0;TCPMESSAGELEN];
 
-#[derive(Copy, Clone,PartialEq)]
+#[derive(Copy, Clone, PartialEq, Serialize, Deserialize)]
 pub struct DataStruct {
     pub cinta1: bool,
     pub cinta2: bool,
@@ -13,15 +15,18 @@ pub struct DataStruct {
 }
 
 pub fn to_bytes(struct_in: DataStruct) -> TcpMessage {
-    let mut bytes_ret: TcpMessage = [0;1];
-    if struct_in.cinta1     { bytes_ret[0] |= 0x01; };
+    //let mut bytes_ret: TcpMessage = [0;1];
+    /*if struct_in.cinta1     { bytes_ret[0] |= 0x01; };
     if struct_in.cinta2     { bytes_ret[0] |= 0x02; };
     if struct_in.pogos      { bytes_ret[0] |= 0x04; };
     if struct_in.selector   { bytes_ret[0] |= 0x08; };
     if struct_in.sensor1    { bytes_ret[0] |= 0x10; };
-    if struct_in.sensor2    { bytes_ret[0] |= 0x20; };
+    if struct_in.sensor2    { bytes_ret[0] |= 0x20; };*/
 
-    bytes_ret
+    //bytes_ret
+
+    serde_json::to_string(&struct_in).expect("Falló serde").as_bytes().try_into().expect("Falló serde2")
+
 }
 
 pub trait Convert {
@@ -35,7 +40,7 @@ impl Convert for DataStruct {
 }
 
 pub fn from_bytes(bytes_in: &[u8]) -> DataStruct {
-    let struct_ret = DataStruct {
+    /*let struct_ret = DataStruct {
         cinta1:      (bytes_in[0] & 0x01) != 0 ,
         cinta2:      (bytes_in[0] & 0x02) != 0 ,
         pogos:      (bytes_in[0] & 0x04) != 0 ,
@@ -44,5 +49,7 @@ pub fn from_bytes(bytes_in: &[u8]) -> DataStruct {
         sensor2:     (bytes_in[0] & 0x20) != 0 ,
     };
 
-    struct_ret
+    struct_ret*/
+
+    serde_json::from_slice(bytes_in).expect("Falló serde Deserialize")
 }
